ad789c7f9ac0e9109921d1449b6499147eb914e4
mdneuzerling <david@neuzerling.com>
2021-12-02 08:24:19 +1100

    day01 - simplify comment

--- 01/code.jl
+++ 01/code.jl
@@ -7,9 +7,16 @@ using ShiftedArrays
     map(x -> parse(Int32, x), _) # convert strings to integers
     _ .- lag(_) # compute difference with previous value
     _[2:end] # remove first element, which will be missing
-    filter(x -> x > 0, _) # filter to positive values, which are the increases in value
+    filter(x -> x > 0, _) # filter to the increases in value
     length # now count
 end
+
+
+
+
+
+
+
 
 # PART 2
 @chain "01/data" begin
